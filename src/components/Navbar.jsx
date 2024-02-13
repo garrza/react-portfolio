a5d708ec46c6ae94b1ca587e07153ce34e70a76d
Ramiro <GARZA.RAMIROV@GMAIL.COM>
2024-02-13 10:00:32 -0600

    Debug tailwind, continue navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,16 +4,43 @@ import { Link } from 'react-router-dom';
 import { styles } from '../styles';
 import { navLinks } from '../constants';
 import { logo, menu, close } from '../assets';
+import { act } from '@react-three/fiber';
 
 const Navbar = () => {
+  const [active, setActive] = useState("");
+
   return (
     <nav
      className={`${styles.paddingX} 
      w-full flex items-center py-5 fixed top-0 z-20
      bg-primary`}>
       <div className="flex justify-between items-center w-full m-w-7xl mx-auto">
-        <Link to="/" className="flex items-center gap-2">
+        <Link 
+        to="/" 
+        className="flex items-center gap-2"
+        onClick={() => {
+          setActive("");
+          window.scrollTo(0, 0);
+        }}>
+          <img 
+          src={logo} 
+          alt="logo" 
+          className="w-9 h-9 object-contain" />
+          <p className='text-white text-[30px] boldtitle cursor-pointer'>ramiro garza</p>
         </Link>
+        <ul className='list-none  sm-flex
+        flex-row gap-10'>
+          {navLinks.map((link) => ( 
+             <li
+             key={link.id}
+             className={`${
+              active === link.title
+              ? "text-blue" : "text-black"
+             } hover:text-blue text-[18px] font-medium cursor-pointer`}>
+               <a href={`#${link.id}`}>{link.title}</a>
+             </li>
+          ))}
+        </ul>
       </div>
     </nav>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#050816",
+        primary: "blue",
         secondary: "#aaa6c3",
         tertiary: "#151030",
         "black-100": "#100d25",
