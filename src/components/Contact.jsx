87331acc543e158a50b0731f4be3ee0ea88e089a
Ramiro <GARZA.RAMIROV@GMAIL.COM>
2024-02-20 16:05:55 -0600

    Add env file

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,16 +33,16 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
-        {
-          from_name: form.name,
-          to_name: "Ramiro Garza",
-          from_email: form.email,
-          to_email: "garza.ramirov@gmail.com",
-          message: form.message,
-        },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        process.env.REACT_APP_EMAILJS_SERVICE_ID,
+        process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+    {
+      from_name: form.name,
+      to_name: "Ramiro Garza",
+      from_email: form.email,
+      to_email: "garza.ramirov@gmail.com",
+      message: form.message,
+    },
+        process.env.REACT_APP_EMAILJS_USER_ID
       )
       .then(
         () => {
