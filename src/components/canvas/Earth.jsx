021b2afea5a2baf8f0c03ce5fd4004489d52701b
Ramiro <GARZA.RAMIROV@GMAIL.COM>
2024-02-20 15:47:56 -0600

    Remove stars 3d model, complete contact section

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -5,10 +5,10 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Earth = () => {
-  const earth = useGLTF("./planet/scene.gltf");
+  const earth = useGLTF("./planet/scene.glb");
 
   return (
-    <primitive object={earth.scene} scale={2.5} position-y={0} rotation-y={0} />
+    <primitive object={earth.scene} scale={2.3} position-y={0} rotation-y={0} />
   );
 };
 
@@ -24,8 +24,11 @@ const EarthCanvas = () => {
         near: 0.1,
         far: 200,
         position: [-4, 3, 6],
+        
       }}
     >
+      <ambientLight intensity={1} />
+      <directionalLight position={[0, 0, 0.05]} />
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
           autoRotate
